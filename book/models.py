cc3ff65625fe2de92771467f989cfa6ed48e72f7
Eugene Rabenko <qwer09846@gmail.com>
2023-12-14 13:48:10 +0200

    add image field

--- book/models.py
+++ book/models.py
@@ -1,8 +1,18 @@
+import os
+import uuid
 from enum import Enum
 
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from enumfields import EnumField
+
+
+def book_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/movies/", filename)
 
 
 class Book(models.Model):
@@ -15,6 +25,7 @@ class Book(models.Model):
     cover = EnumField(CoverChoices, default=CoverChoices.HARD, max_length=18)
     inventory = models.PositiveIntegerField(default=0)
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(null=True, upload_to=book_image_file_path)
 
     class Meta:
         ordering = ["title"]
