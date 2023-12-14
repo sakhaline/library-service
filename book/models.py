e42e2c0bd78ca4c92ef0db92a36420916a1be042
Eugene Rabenko <qwer09846@gmail.com>
2023-12-14 13:09:36 +0200

    change type of cover field to enum-field

--- book/models.py
+++ book/models.py
@@ -1,14 +1,18 @@
+from enum import Enum
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from enumfields import EnumField
 
 
 class Book(models.Model):
-    class Cover(models.TextChoices):
-        HARD = "H", _("Hard cover")
-        SOFT = "S", _("Soft cover")
+    class CoverChoices(Enum):
+        HARD = "H", _("Hardcover")
+        SOFT = "S", _("Softcover")
+
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=4, choices=Cover, default=Cover.HARD)
+    cover = EnumField(CoverChoices, default=CoverChoices.HARD, max_length=18)
     inventory = models.PositiveIntegerField(default=0)
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
 
