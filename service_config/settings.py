6f9a30ca71389a892997775aa568a315ebb466f6
sakhaline <alexandra.ivva@gmail.com>
2023-12-14 10:41:09 +0200

    Initial Django project

--- service_config/settings.py
+++ service_config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "libraryService.urls"
+ROOT_URLCONF = "service_config.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "libraryService.wsgi.application"
+WSGI_APPLICATION = "service_config.wsgi.application"
 
 
 # Database
