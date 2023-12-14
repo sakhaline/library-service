8f2c735b456a18c1e19c9ea94c0b59dce23b072c
Serhii <martinenko.sergiy@gmail.com>
2023-12-14 14:14:11 +0200

    update models return date field

--- borrowing/models.py
+++ borrowing/models.py
@@ -7,6 +7,6 @@ from book.models import Book
 class Borrowing(models.Model):
     borrow_date = models.DateTimeField(auto_now_add=True)
     expected_return_date = models.DateTimeField()
-    actual_return_date = models.DateTimeField(blank=True)
+    actual_return_date = models.DateTimeField(blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
