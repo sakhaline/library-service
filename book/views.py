04375596bd18e4484409649dab89634f502e063b
Eugene Rabenko <qwer09846@gmail.com>
2023-12-14 16:24:29 +0200

    add views to views.py

--- book/views.py
+++ book/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from book.models import Book
+from book.serializers import (
+    BookListSerializer,
+    BookDetailSerializer,
+    BookImageSerializer,
+)
+
+
+class BookListViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Book.objects.all()
+    serializer_class = BookListSerializer
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BookDetailSerializer
+
+        if self.action == "upload_image":
+            return BookImageSerializer
+
+        return BookListSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific book"""
+        movie = self.get_object()
+        serializer = self.get_serializer(movie, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
