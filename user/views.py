4c95a3efede5e06ae5d3c89db0ec8a3dfd31f8f9
sakhaline <alexandra.ivva@gmail.com>
2023-12-15 11:32:13 +0200

    Add permission to ManageUserView

--- user/views.py
+++ user/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializers import AuthTokenSerializer, UserSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -16,6 +17,7 @@ class CreateTokenView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
