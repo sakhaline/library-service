fc64acf4bde600f34c21c1fff4b3a7227abe1997
sakhaline <alexandra.ivva@gmail.com>
2023-12-17 05:04:30 +0200

    Configure Celery

--- service_config/settings.py
+++ service_config/settings.py
@@ -20,8 +20,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "rest_framework_simplejwt",
+    "django_celery_beat",
+
     "book",
     "payment",
     "borrowing",
