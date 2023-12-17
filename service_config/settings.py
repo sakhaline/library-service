2160181b5df9c0118bc362be92e9d721dee317d3
sakhaline <alexandra.ivva@gmail.com>
2023-12-17 07:48:47 +0200

    Change AUTH_HEADER_NAME

--- service_config/settings.py
+++ service_config/settings.py
@@ -123,6 +123,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
+    "AUTH_HEADER_NAME": "Auth",
 }
 
 LANGUAGE_CODE = "en-us"
