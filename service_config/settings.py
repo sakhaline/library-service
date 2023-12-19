dad806f15611aeba8d63366964e1609961ecaf2e
sakhaline <alexandra.ivva@gmail.com>
2023-12-19 21:56:53 +0200

    Update payment endpoint

--- service_config/settings.py
+++ service_config/settings.py
@@ -102,8 +102,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
 
